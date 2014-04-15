bccc83af7b5643bebeccefc49bf6cc6c8e7802f2
Chris North <chris.north@astro.cf.ac.uk>
2014-04-15 18:43:48 +0100

    edited to use spireBands(). Doesb't use sources_dev properly

--- HIPE/dev/SemiExtendedBundle_dev.py
+++ HIPE/dev/SemiExtendedBundle_dev.py
@@ -94,7 +94,7 @@ import sources_dev as srcMod
 #-------------------------------------------------------------------------------
 
 
-def calcBeamSrcMonoArea(srcKey,verbose=False):
+def calcBeamSrcMonoArea(src,verbose=False):
     #calculate monochromatic beam areas using full or simple beam treatment
     #print '\nCalculating monochromatic beam areas...'
 
@@ -108,7 +108,7 @@ def calcBeamSrcMonoArea(srcKey,verbose=False):
         arcsec2Sr = (PI/(60.*60.*180))**2
     
     try:
-        beamMonoSrcArea[srcKey]
+        beamMonoSrcArea[src.key]
         if (verbose):print'Using existing monochromatic areas (key %s)'%srcKey
         #exists, don't recalculate
     except:
@@ -128,7 +128,7 @@ def calcBeamSrcMonoArea(srcKey,verbose=False):
         
         gamma = beamProfs.meta['gamma'].double
         beamMonoSrcArea[srcKey] = {'PSW': Double.NaN, 'PMW': Double.NaN, 'PLW': Double.NaN}
-        for band in spireBands:
+        for band in spireBands():
             if (verbose):print 'Calculating monochromatic beam areas for %s source for %s band'%(srcKey,band)
             #monochromatic beam areas
             beamMonoSrcArea[srcKey][band] = spireMonoSrcAreas(getSpireFreq(), beamProfs, 
@@ -136,7 +136,7 @@ def calcBeamSrcMonoArea(srcKey,verbose=False):
         if (verbose):print 'All beam areas calculated for %s source'%srcKey
     return beamMonoSrcArea[srcKey]
 
-def calcKColESrc(alphaK,srcKey,verbose=False,table=False):
+def calcKColESrc(alphaK,src,verbose=False,table=False):
     #-----------------------------------------------------------------------
     #print '\nCalculating extended source colour correction parameters over alpha...'
     #cal=getCal(cal=cal,calPool=calPool,calFile=calFile)
@@ -144,7 +144,7 @@ def calcKColESrc(alphaK,srcKey,verbose=False,table=False):
     #spireFilt=getSpireFilt(cal)
 
     k4E_Tot=calcKMonE()
-
+    print k4E_Tot
     try:
         na=len(alphaK)
         aList=True
@@ -155,20 +155,20 @@ def calcKColESrc(alphaK,srcKey,verbose=False,table=False):
         # alphaK is scalar
         kColESrc = {'PSW': Double.NaN, 'PMW': Double.NaN, 'PLW': Double.NaN}
         #kBeamK = calcKBeam(alphaK)
-        for band in spireBands:
+        for band in spireBands():
             k4EaTot_x=calcSpireKcorr(getSpireRefFreq()[band], getSpireFreq(),\
              getSpireFilt()[band], BB=False, alpha=alphaK,\
-             ext=True, monoArea=calcBeamSrcMonoArea(srcKey,verbose=verbose)[band])[0]/1.e6
+             ext=True, monoArea=calcBeamSrcMonoArea(src,verbose=verbose)[band])[0]/1.e6
             kColESrc[band] = k4EaTot_x / k4E_Tot[band]
         if (verbose): print 'Calculated KColE for alpha=%f and source Width: '%alphaK,kColESrc
     else:
         # alphaK is list
         kColESrc = {'PSW': Double1d(na,Double.NaN), 'PMW': Double1d(na,Double.NaN), 'PLW': Double1d(na,Double.NaN)}
         for a in range(na):
-            for band in spireBands:
+            for band in spireBands():
                 k4EaTot_x=calcSpireKcorr(getSpireRefFreq()[band], getSpireFreq(),\
                  getSpireFilt()[band], BB=False, alpha=alphaK[a],\
-                 ext=True, monoArea=calcBeamSrcMonoArea(srcKey,verbose=verbose)[band])[0]/1.e6
+                 ext=True, monoArea=calcBeamSrcMonoArea(src,verbose=verbose)[band])[0]/1.e6
                 kColESrc[band][a] = k4EaTot_x / k4E_Tot[band]
             if (verbose): print 'Calculated KColE for alpha=%f: '%alphaK[a],kColESrc["PSW"][a],kColESrc["PMW"][a],kColESrc["PLW"][a]
 
@@ -180,17 +180,19 @@ def calcKColESrc(alphaK,srcKey,verbose=False,table=False):
         kColE_table=TableDataset()
         kColE_table.setDescription("Partially Extended Source Colour Correction (Spectral Index)")
         kColE_table.addColumn("alpha",Column(Double1d(alphaK)))
-        for band in spireBands:
+        for band in spireBands():
             kColE_table.addColumn(band,Column(kColESrc[band]))
         return kColE_table
 
 def semiExtendedTest():
 
     #make Gaussian source
+    print 'start'
     alphaK=2.0
     srcWidths=[1.,10.,100.,1000]
     KColE_full=calcKColE(alphaK,verbose=True)
+    print 'full',KColE_full
     KColE_partial=[]
     for wid in srcWidths:
-        srcKey=srcMod.setSrc('Gaussian',[wid])
-        KColE_partial.append(calcKColESrc(alphaK,srcKey,verbose=True,table=False))
+        src=srcMod.Source('Gaussian',[wid])
+        KColE_partial.append(calcKColESrc(alphaK,src,verbose=True,table=False))
